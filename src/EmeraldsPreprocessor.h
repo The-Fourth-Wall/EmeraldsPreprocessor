5b3ce8ec4e7f59337cca45180dbd63904b56db97
oblivious <atha@fourth-wall.org>
2025-03-04 01:10:06 +0200

    [master] - Aligned comments with c89.

--- src/EmeraldsPreprocessor.h
+++ src/EmeraldsPreprocessor.h
@@ -49,10 +49,10 @@
   #else
     #define PREPROCESSOR_C_VERSION 1989
   #endif
-// Older GCC/Clang without __STDC_VERSION__
+/* Older GCC/Clang without __STDC_VERSION__ */
 #elif defined(__GNUC__) || defined(__clang__)
   #define PREPROCESSOR_C_VERSION 1989
-// Oracle Solaris Studio compiler
+/* Oracle Solaris Studio compiler */
 #elif defined(__SUNPRO_C)
   #if __SUNPRO_C >= 0x5150
     #define PREPROCESSOR_C_VERSION 1999
@@ -61,32 +61,32 @@
   #else
     #define PREPROCESSOR_C_VERSION 1989
   #endif
-// IBM compilers
+/* IBM compilers */
 #elif defined(__xlC__) || defined(__IBMC__) || defined(__IBMCPP__)
   #if __xlC__ >= 0x0A01
     #define PREPROCESSOR_C_VERSION 1999
   #else
     #define PREPROCESSOR_C_VERSION 1989
   #endif
-// HP-UX compilers
+/* HP-UX compilers */
 #elif defined(__HP_cc) || defined(__HP_aCC)
-  // HP's compilers are generally C95 compliant
+  /* HP's compilers are generally C95 compliant */
   #define PREPROCESSOR_C_VERSION 1995
-// IAR Embedded Workbench
+/* IAR Embedded Workbench */
 #elif defined(__IAR_SYSTEMS_ICC__)
   #if __IAR_SYSTEMS_ICC__ >= 8
     #define PREPROCESSOR_C_VERSION 1999
   #else
     #define PREPROCESSOR_C_VERSION 1989
   #endif
-// Texas Instruments compilers
+/* Texas Instruments compilers */
 #elif defined(__TI_COMPILER_VERSION__)
   #if __TI_COMPILER_VERSION__ >= 6000000
     #define PREPROCESSOR_C_VERSION 1999
   #else
     #define PREPROCESSOR_C_VERSION 1989
   #endif
-// GCC in strict ANSI mode
+/* GCC in strict ANSI mode */
 #elif defined(__GNUC__) && defined(__STRICT_ANSI__)
   #define PREPROCESSOR_C_VERSION 1989
 #else
